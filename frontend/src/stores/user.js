9e4d9033bd8cd69c389b14734c1a859f730561e7
hlzw <318810862@qq.com>
2026-01-30 14:58:03 +0800

    make Menus

---
+++ frontend/src/stores/user.js
@@ -0,0 +1,51 @@
+
+// 写完后端后，维护登录状态--全局变量，全局变量一般存在stores
+
+import {defineStore} from "pinia";
+import {ref} from "vue";
+
+export const useUserStore = defineStore('user', ()=>{
+    const id = ref('1')
+    const username = ref('222')
+    const photo = ref('')
+    const profile = ref('111')
+    const accessToken = ref('111')
+
+    //判断是否登录
+    function isLogin(){
+        return !!accessToken.value
+    }
+
+    //设置accesstoken
+    function setAccessToken(token){
+        accessToken.value = token
+    }
+
+    //保存用户信息
+    function setUserInfo(data){
+        id.value = data.user_id
+        username.value = data.username
+        photo.value = data.photo
+        profile.value = data.profile
+    }
+
+    function logout(){
+        id.value = 0
+        username.value = ''
+        photo.value = ''
+        accessToken.value = ''
+        profile.value = ''
+    }
+
+    return {
+        id,
+        username,
+        photo,
+        profile,
+        accessToken,
+        isLogin,
+        setAccessToken,
+        setUserInfo,
+        logout,
+    }
+})
