1c3a053c70be4e4db9b43bce002d11196fa6ba30
hlzw <318810862@qq.com>
2026-02-12 21:55:58 +0800

    打补丁

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,6 +1,6 @@
 from web.views.user.account.get_user_info import GetUserInfoView
 from web.views.user.account.login import LoginView
-from django.urls import path
+from django.urls import path, re_path
 from web.views.index import index
 from web.views.user.account.logout import LogoutView
 from web.views.user.account.register import RegisterView
@@ -13,5 +13,8 @@ urlpatterns = [
     path('api/user/account/get_user_info/', GetUserInfoView.as_view(), name='get_user_info'),
     path('', index),
 
-    # re_path(r'^(?!media/|static/|assets/).*$', index)
+    # 正则表达式排除所有后端路由并返回前端路由index.html前端路由（有router)
+    #eg: friend下路由匹配到最后一项返回index.html（前端view),view里有RouterView返回对应组件
+    re_path(r'^(?!media/|static/|assets/).*$', index)
+
 ]
